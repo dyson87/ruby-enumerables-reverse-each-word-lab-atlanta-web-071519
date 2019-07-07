dd24d36be5bf6e19eb3efce561f59c67582068d7
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-07 16:55:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(sentence1)
-  sentence1 = sentence1.split('')
-  reversed_sentence1 = []
-  sentence1.each do
-    char
-    reversed_sentence1.unshift(char)
-  end
-  return reversed_sentence1.join('')
+ 
 end
